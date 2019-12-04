9f0c4c39b91887379eda747e3b7a7bf954acbf00
Jeff Remillard <jdr@remgant.net>
2019-12-04 07:14:34 -0500

    Adding ExpressionBuilder. #11

---
+++ src/test/java/net/remgant/tools/parser/test/ExpressionBuilderSampleParserTest.java
@@ -0,0 +1,53 @@
+package net.remgant.tools.parser.test;
+
+import net.remgant.tools.parser.ParserResult;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ExpressionBuilderSampleParserTest {
+    @Test
+    public void testSingleInt() throws Exception {
+        ExpressionBuilderSampleParser parser = new ExpressionBuilderSampleParser();
+        ParserResult c = parser.parse("a = 1;");
+        System.out.println(c);
+        assertTrue(c instanceof ExpressionBuilderSampleParser.AssignmentStatement);
+        ExpressionBuilderSampleParser.AssignmentStatement assignmentStatement = (ExpressionBuilderSampleParser.AssignmentStatement) c;
+        assertEquals("a", assignmentStatement.getTarget());
+        assertEquals("1", assignmentStatement.getNode().toString());
+    }
+
+    @Test
+    public void testSimpleExpression() throws Exception {
+        ExpressionBuilderSampleParser parser = new ExpressionBuilderSampleParser();
+        ParserResult c = parser.parse("a = b + c;");
+        System.out.println(c);
+        assertTrue(c instanceof ExpressionBuilderSampleParser.AssignmentStatement);
+        ExpressionBuilderSampleParser.AssignmentStatement assignmentStatement = (ExpressionBuilderSampleParser.AssignmentStatement) c;
+        assertEquals("a", assignmentStatement.getTarget());
+        assertEquals("b c +", assignmentStatement.getNode().toString());
+    }
+
+    @Test
+    public void testExpressionThreeArgs() throws Exception {
+        ExpressionBuilderSampleParser parser = new ExpressionBuilderSampleParser();
+        ParserResult c = parser.parse("a = b + c + d;");
+        System.out.println(c);
+        assertTrue(c instanceof ExpressionBuilderSampleParser.AssignmentStatement);
+        ExpressionBuilderSampleParser.AssignmentStatement assignmentStatement = (ExpressionBuilderSampleParser.AssignmentStatement) c;
+        assertEquals("a", assignmentStatement.getTarget());
+        assertEquals("b c + d +", assignmentStatement.getNode().toString());
+    }
+
+    @Test
+    public void testExpressionWithParen() throws Exception {
+        ExpressionBuilderSampleParser parser = new ExpressionBuilderSampleParser();
+        ParserResult c = parser.parse("a = b + (c + d);");
+        System.out.println(c);
+        assertTrue(c instanceof ExpressionBuilderSampleParser.AssignmentStatement);
+        ExpressionBuilderSampleParser.AssignmentStatement assignmentStatement = (ExpressionBuilderSampleParser.AssignmentStatement) c;
+        assertEquals("a", assignmentStatement.getTarget());
+        assertEquals("b c d + +", assignmentStatement.getNode().toString());
+    }
+}
